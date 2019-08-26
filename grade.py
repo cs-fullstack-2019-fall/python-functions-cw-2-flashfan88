ba363976cc70279042475fdf1e983fed8e5b6f41
flashfan88 <lavarjohnson20@gmail.com>
2019-08-26 13:02:28 -0500

    ugh

---
+++ grade.py
@@ -0,0 +1,17 @@
+# Problem 1:
+# Create a function that will ask the user for a number.
+# Use the function to get two numbers from the user,
+# then pass the two numbers to a function. Add, subtract, multiple,
+# and divide the numbers.
+
+
+
+def main():
+
+number =
+
+
+userInput = int(input("Enter a number"))
+userInput2 = int(input("Enter Another number"))
+main()
+# ???
